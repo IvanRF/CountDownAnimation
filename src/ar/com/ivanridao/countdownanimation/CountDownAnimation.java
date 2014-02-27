1003c74c58e0a1e13736f6b3052cc8bbf176230a
Ivan <ivanridao@yahoo.com.ar>
2014-02-27 18:25:31 -0300

    Format code

--- src/ar/com/ivanridao/countdownanimation/CountDownAnimation.java
+++ src/ar/com/ivanridao/countdownanimation/CountDownAnimation.java
@@ -25,70 +25,75 @@ import android.widget.TextView;
 public class CountDownAnimation {
 
 	private TextView mTextView;
-    private Animation mAnimation;
-    private int mStartCount;
-    private int mCurrentCount;
-    private CountDownListener mListener;
-    
-    private Handler mHandler = new Handler();
+	private Animation mAnimation;
+	private int mStartCount;
+	private int mCurrentCount;
+	private CountDownListener mListener;
 
-    private final Runnable mCountDown = new Runnable() {
-        public void run() {
-        	if (mCurrentCount > 0) {
-        		mTextView.setText(mCurrentCount+"");
-        		mTextView.startAnimation(mAnimation);
-	        	mCurrentCount--;
-	       	} else {
-	       		mTextView.setVisibility(View.GONE);
-	       		if (mListener != null)
-	       			mListener.onCountDownEnd(CountDownAnimation.this);
-	       	}
-        }
-    };
-    
+	private Handler mHandler = new Handler();
+
+	private final Runnable mCountDown = new Runnable() {
+		public void run() {
+			if (mCurrentCount > 0) {
+				mTextView.setText(mCurrentCount + "");
+				mTextView.startAnimation(mAnimation);
+				mCurrentCount--;
+			} else {
+				mTextView.setVisibility(View.GONE);
+				if (mListener != null)
+					mListener.onCountDownEnd(CountDownAnimation.this);
+			}
+		}
+	};
+
 	public CountDownAnimation(TextView textView, int startCount) {
 		this.mTextView = textView;
 		this.mStartCount = startCount;
-		
+
 		mAnimation = new AlphaAnimation(1.0f, 0.0f);
 		mAnimation.setDuration(1000);
 	}
-    
+
 	public void start() {
 		mHandler.removeCallbacks(mCountDown);
-		
-		mTextView.setText(mStartCount+"");
+
+		mTextView.setText(mStartCount + "");
 		mTextView.setVisibility(View.VISIBLE);
-		
+
 		mCurrentCount = mStartCount;
-		
+
 		mHandler.post(mCountDown);
 		for (int i = 1; i <= mStartCount; i++) {
 			mHandler.postDelayed(mCountDown, i * 1000);
 		}
 	}
-	
+
 	public void setAnimation(Animation animation) {
 		this.mAnimation = animation;
-		if(mAnimation.getDuration() == 0)
+		if (mAnimation.getDuration() == 0)
 			mAnimation.setDuration(1000);
 	}
-	
+
 	public void setCountDownListener(CountDownListener listener) {
-        mListener = listener;
-    }
-	
+		mListener = listener;
+	}
+
 	/**
-     * <p>A count down listener receives notifications from a count down animation.
-     * Notifications indicate count down animation related events, such as the end
-     * of the animation.</p>
-     */
+	 * <p>
+	 * A count down listener receives notifications from a count down animation.
+	 * Notifications indicate count down animation related events, such as the
+	 * end of the animation.
+	 * </p>
+	 */
 	public static interface CountDownListener {
 		/**
-         * <p>Notifies the end of the count down animation.</p>
-         *
-         * @param animation The count down animation which reached its end.
-         */
-	    void onCountDownEnd(CountDownAnimation animation);
+		 * <p>
+		 * Notifies the end of the count down animation.
+		 * </p>
+		 * 
+		 * @param animation
+		 *            The count down animation which reached its end.
+		 */
+		void onCountDownEnd(CountDownAnimation animation);
 	}
 }
