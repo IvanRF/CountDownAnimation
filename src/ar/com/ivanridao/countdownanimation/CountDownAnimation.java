68a5d57f835f291dae738908db31d4fd9002cb86
Ivan <ivanridao@yahoo.com.ar>
2014-03-01 12:56:19 -0300

    Added gets, sets and doc comments

--- src/ar/com/ivanridao/countdownanimation/CountDownAnimation.java
+++ src/ar/com/ivanridao/countdownanimation/CountDownAnimation.java
@@ -22,6 +22,12 @@ import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.widget.TextView;
 
+/**
+ * Defines a count down animation to be shown on a {@link TextView }.
+ * 
+ * @author Ivan Ridao Freitas
+ * 
+ */
 public class CountDownAnimation {
 
 	private TextView mTextView;
@@ -46,6 +52,21 @@ public class CountDownAnimation {
 		}
 	};
 
+	/**
+	 * <p>
+	 * Creates a count down animation in the <var>textView</var>, starting from
+	 * <var>startCount</var>.
+	 * </p>
+	 * <p>
+	 * By default, the class defines a fade out animation, which uses
+	 * {@link AlphaAnimation } from 1 to 0.
+	 * </p>
+	 * 
+	 * @param textView
+	 *            The view where the count down will be shown
+	 * @param startCount
+	 *            The starting count number
+	 */
 	public CountDownAnimation(TextView textView, int startCount) {
 		this.mTextView = textView;
 		this.mStartCount = startCount;
@@ -54,6 +75,9 @@ public class CountDownAnimation {
 		mAnimation.setDuration(1000);
 	}
 
+	/**
+	 * Starts the count down animation.
+	 */
 	public void start() {
 		mHandler.removeCallbacks(mCountDown);
 
@@ -68,6 +92,9 @@ public class CountDownAnimation {
 		}
 	}
 
+	/**
+	 * Cancels the count down animation.
+	 */
 	public void cancel() {
 		mHandler.removeCallbacks(mCountDown);
 
@@ -75,28 +102,59 @@ public class CountDownAnimation {
 		mTextView.setVisibility(View.GONE);
 	}
 
+	/**
+	 * Sets the animation used during the count down. If the duration of the
+	 * animation for each number is not set, one second will be defined.
+	 */
 	public void setAnimation(Animation animation) {
 		this.mAnimation = animation;
 		if (mAnimation.getDuration() == 0)
 			mAnimation.setDuration(1000);
 	}
 
+	/**
+	 * Returns the animation used during the count down.
+	 */
+	public Animation getAnimation() {
+		return mAnimation;
+	}
+
+	/**
+	 * Sets a new starting count number for the count down animation.
+	 * 
+	 * @param startCount
+	 *            The starting count number
+	 */
+	public void setStartCount(int startCount) {
+		this.mStartCount = startCount;
+	}
+
+	/**
+	 * Returns the starting count number for the count down animation.
+	 */
+	public int getStartCount() {
+		return mStartCount;
+	}
+
+	/**
+	 * Binds a listener to this count down animation. The count down listener is
+	 * notified of events such as the end of the animation.
+	 * 
+	 * @param listener
+	 *            The count down listener to be notified
+	 */
 	public void setCountDownListener(CountDownListener listener) {
 		mListener = listener;
 	}
 
 	/**
-	 * <p>
 	 * A count down listener receives notifications from a count down animation.
 	 * Notifications indicate count down animation related events, such as the
 	 * end of the animation.
-	 * </p>
 	 */
 	public static interface CountDownListener {
 		/**
-		 * <p>
 		 * Notifies the end of the count down animation.
-		 * </p>
 		 * 
 		 * @param animation
 		 *            The count down animation which reached its end.
